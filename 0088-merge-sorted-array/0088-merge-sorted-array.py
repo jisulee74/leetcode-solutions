f4694c7c01cb9f3abab66de4356c3870aac95754
jisulee74 <lgs010704@gmail.com>
2026-08-31 16:54:14 +0900

    Time: 0 ms (100%), Space: 12.5 MB (21.31%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -8,4 +8,20 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
         
-        nums1[:] = sorted(nums1[:m] + nums2[:n])
+        p1 = m - 1 
+        p2 = n - 1
+        p = m + n - 1
+
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+        
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
