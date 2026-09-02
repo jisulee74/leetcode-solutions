429b02bb0a6cf540b2d9703cbfb40f9c7aa5b876
jisulee74 <lgs010704@gmail.com>
2026-09-02 13:21:05 +0900

    Time: 17 ms (19.8%), Space: 12.6 MB (5.7%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -5,11 +5,15 @@ class Solution(object):
         :rtype: bool
         """
         count = 0
+        break_all = False
         
         for i in range(0, len(arr)):
             for j in range(0, len(arr)):
                 if arr[i] == 2 * arr[j] and i != j:
                     count += 1
+                    break_all = True
                     break
+            if break_all:
+                break
         
         return True if count != 0 else False
