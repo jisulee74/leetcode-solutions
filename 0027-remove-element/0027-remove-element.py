66ee5ac1bb39aec541a623a8d554b62765e9ef4c
jisulee74 <lgs010704@gmail.com>
2026-09-02 10:47:25 +0900

    Time: 0 ms (100%), Space: 12.5 MB (17.43%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -5,9 +5,7 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        # for i in range(0, len(nums)):
-        #     if nums[i] == val:
-        #         nums[i] = nums[i+1]
+
         nums[:] = [x for x in nums if x != val]
         
         return len(nums)
