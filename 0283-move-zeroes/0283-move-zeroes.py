235392001cd73dc2c9f8c36214751bd7cd8e4c22
jisulee74 <lgs010704@gmail.com>
2026-09-08 09:27:07 +0900

    Time: 5 ms (67.93%), Space: 13.7 MB (20.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -8,7 +8,7 @@ class Solution(object):
         
         for i in range(len(nums)):
             if nums[i] != 0:
-                # 자기 자신과의 교환을 피하기 위해 인덱스가 다를 때만 스왑
+                # Avoid swapping with itself to optimize performance
                 if i != last_non_zero_found_at:
                     nums[last_non_zero_found_at], nums[i] = nums[i], nums[last_non_zero_found_at]
                 last_non_zero_found_at += 1
