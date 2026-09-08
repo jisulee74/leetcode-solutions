a16d1cf9acbf71c938204605240b7bf051815622
jisulee74 <lgs010704@gmail.com>
2026-09-08 09:26:44 +0900

    Time: 4 ms (79.37%), Space: 13.6 MB (20.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        index = 0
+        last_non_zero_found_at = 0
         
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[index] = nums[i]
-                index += 1
-        while(index < len(nums)):
-            nums[index] = 0
-            index += 1
+                # 자기 자신과의 교환을 피하기 위해 인덱스가 다를 때만 스왑
+                if i != last_non_zero_found_at:
+                    nums[last_non_zero_found_at], nums[i] = nums[i], nums[last_non_zero_found_at]
+                last_non_zero_found_at += 1
