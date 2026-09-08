5e304ee3828cf0baaffdd835aaaa2bd507418be3
jisulee74 <lgs010704@gmail.com>
2026-09-08 09:27:42 +0900

    Time: 15 ms (14.23%), Space: 13.6 MB (50.17%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        index = 0
+        last_non_zero_found_at = 0
         
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[index] = nums[i]
-                index += 1
-        while(index < len(nums)):
-            nums[index] = 0
-            index += 1
+                # Avoid swapping with itself to optimize performance
+                if i != last_non_zero_found_at:
+                    nums[last_non_zero_found_at], nums[i] = nums[i], nums[last_non_zero_found_at]
+                last_non_zero_found_at += 1
