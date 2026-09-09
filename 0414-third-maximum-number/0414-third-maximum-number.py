99e31b2b1ff82e64b73331a2d3673edb2336f2dc
jisulee74 <lgs010704@gmail.com>
2026-09-09 15:33:15 +0900

    Time: 0 ms (100%), Space: 13 MB (67.93%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -4,9 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        sorted_nums = sorted(set(nums), reverse=True)
-        
-        if len(sorted_nums) < 3:
-            return sorted_nums[0]
-        else:
-            return sorted_nums[2]
+        m1 = m2 = m3 = None
+
+        for num in nums:
+            if num in (m1, m2, m3):
+                continue
+
+            if m1 is None or num > m1:
+                m3, m2, m1 = m2, m1, num
+            elif m2 is None or num > m2:
+                m3, m2 = m2, num
+            elif m3 is None or num > m3:
+                m3 = num
+
+        return m3 if m3 is not None else m1                
