d559aebdef5d7d477e9ab3a79f56be05186580eb
jisulee74 <lgs010704@gmail.com>
2026-09-09 14:13:03 +0900

    Time: 28 ms (86.64%), Space: 30.4 MB (7.03%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -4,15 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        char = {i:0 for i in range(1, n+1)}
-        target_value = 0
-        
-        for num in nums:
-            if char[num] != 0:
-                char[num] += 1
-            else:
-                char[num] = 1
-                
-        return [key for key, value in char.items() if value == target_value]
+        return list(set(range(1, len(nums)+1))- set(nums))
         
