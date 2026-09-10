30d29a889c587900e5c88b7a4ae81c508efca1e8
jisulee74 <lgs010704@gmail.com>
2026-09-10 11:55:59 +0900

    Time: 0 ms (100%), Space: 12.4 MB (17.26%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,7 +4,12 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        num_str = "".join(map(str, digits))
-        plus_one = int(num_str) + 1
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
         
-        return [int(char) for char in str(plus_one)]
+        digits.insert(0, 1)
+        return digits
