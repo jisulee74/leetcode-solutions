f8f31b9b838950a46a5b5217b803b505bf07ca82
jisulee74 <lgs010704@gmail.com>
2026-09-10 10:50:58 +0900

    Time: 0 ms (100%), Space: 12.3 MB (56.33%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.py
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.py
@@ -4,18 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count = 0 
-        
+        max1 = max2 = -1
+        index = -1
+
         for i, num in enumerate(nums):
-            for j in range(len(nums)):
-                if i != j:
-                    if num < nums[j] * 2:
-                        break
-                    count += 1
-            if count == len(nums) - 1:
-                return i
-            count = 0
-            
-        return -1
+            if num > max1:
+                max2, max1 = max1, num
+                index = i
+            elif num > max2:
+                max2 = num
+        
+        return index if max1 >= max2 * 2 else -1
                     
         
